3e748ab098c38b2d0730876a0631a863628f5088
木辛 <923156033@qq.com>
2020-07-17 11:20:07 +0800

    2

--- skin/sys/js/console.js
+++
@@ -1,7 +0,0 @@
-Console={};
-Console.f_heightChanged =function(options){
-	if (tab)
-		tab.addHeight(options.diff);
-	if (accordion && options.middleHeight - 24 > 0)
-		accordion.setHeight(options.middleHeight - 24);
-};
